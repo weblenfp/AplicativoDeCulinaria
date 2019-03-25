a21b95f255f73fbf71a6b0112d7aa8ab3e1a3a58
Weblen Fernandes Pimentel <weblen.pimentel@userede.com.br>
2019-03-25 19:31:09 -0300

    Saving changes - not working yeat.

--- app/src/main/java/dev/weblen/aplicativodeculinaria/adapters/RecipesAdapter.java
+++ app/src/main/java/dev/weblen/aplicativodeculinaria/adapters/RecipesAdapter.java
@@ -4,14 +4,17 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
+import java.util.Locale;
 
 import dev.weblen.aplicativodeculinaria.R;
 import dev.weblen.aplicativodeculinaria.holders.IngredientsViewHolder;
 import dev.weblen.aplicativodeculinaria.holders.RecipeViewHolder;
 import dev.weblen.aplicativodeculinaria.holders.StepViewHolder;
+import dev.weblen.aplicativodeculinaria.models.Ingredient;
 import dev.weblen.aplicativodeculinaria.models.Recipe;
 import dev.weblen.aplicativodeculinaria.ui.Listeners;
 
@@ -19,6 +22,7 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     private Context                       mContext;
     private List<Recipe>                  mRecipes;
     private Listeners.OnItemClickListener mOnItemClickListener;
+    private Recipe mRecipe;
 
     public RecipesAdapter(Context context, List<Recipe> recipes, Listeners.OnItemClickListener onItemClickListener) {
         this.mContext = context;
@@ -39,10 +43,33 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, final int position) {
+        if (viewHolder instanceof IngredientsViewHolder) {
+            IngredientsViewHolder mViewHolder = (IngredientsViewHolder) viewHolder;
 
+            StringBuilder ingValue = new StringBuilder();
+            for (int i = 0; i < mRecipe.getIngredients().size(); i++) {
+                Ingredient ingredients = mRecipe.getIngredients().get(i);
+                ingValue.append(String.format(Locale.getDefault(), "• %s (%d %s)", ingredients.getIngredient(), ingredients.getQuantity(), ingredients.getMeasure()));
+                if (i != mRecipe.getIngredients().size() - 1)
+                    ingValue.append("\n");
+            }
+
+            ((IngredientsViewHolder) viewHolder).mTvIngredients.setText(ingValue.toString());
+        } else if (viewHolder instanceof StepViewHolder) {
+            StepViewHolder mViewHolder = (StepViewHolder) viewHolder;
+            ((StepViewHolder) viewHolder).mTvStepName.setText(String.valueOf(position - 1) + ".");
+            ((StepViewHolder) viewHolder).mTvStepName.setText(mRecipe.getSteps().get(position - 1).getShortDescription());
+
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mOnItemClickListener != null)
+                        mOnItemClickListener.onItemClick(position - 1);
+                }
+            });
+        }
     }
-
 
     @Override
     public int getItemCount() {
--- app/src/main/java/dev/weblen/aplicativodeculinaria/holders/StepViewHolder.java
+++ app/src/main/java/dev/weblen/aplicativodeculinaria/holders/StepViewHolder.java
@@ -1,11 +1,23 @@
 package dev.weblen.aplicativodeculinaria.holders;
 
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dev.weblen.aplicativodeculinaria.R;
 
 public class StepViewHolder extends RecyclerView.ViewHolder {
-    public StepViewHolder(@NonNull View itemView) {
+    @BindView(R.id.step_order_text)
+    public TextView mTvStepOrder;
+
+    @BindView(R.id.step_name_text)
+    public TextView mTvStepName;
+
+    public StepViewHolder(View itemView) {
         super(itemView);
+
+        ButterKnife.bind(this, itemView);
     }
 }
