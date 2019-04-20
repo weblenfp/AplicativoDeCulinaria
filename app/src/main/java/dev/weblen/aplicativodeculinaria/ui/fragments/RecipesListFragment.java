b0853a293c98311539ddcb4a99bc25a6037acf27
Weblen Fernandes Pimentel <>
2019-04-19 22:18:12 -0300

    Removing TODOs.

--- app/src/main/java/dev/weblen/aplicativodeculinaria/ui/fragments/RecipesListFragment.java
+++ app/src/main/java/dev/weblen/aplicativodeculinaria/ui/fragments/RecipesListFragment.java
@@ -185,7 +185,6 @@ public class RecipesListFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Recipe recipe);
     }
 }
